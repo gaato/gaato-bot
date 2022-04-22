1b3250a3b78437c3ca636476e141a74a95a88adb
gaato <gart@outlook.jp>
2022-04-22 14:21:38 +0900

    ephemeral=False

--- bots/slash.py
+++ bots/slash.py
@@ -25,7 +25,7 @@ bot = discord.Bot()
 async def privacy_policy(ctx: discord.ApplicationContext):
     """Show the privacy policy"""
     with open(BASE_DIR / 'config' / 'privacy-policy.md', 'r') as f:
-        await ctx.respond(f.read(), ephemeral=True)
+        await ctx.respond(f.read())
 
 @bot.slash_command(name='opt-out')
 async def opt_out(ctx: discord.ApplicationContext):
@@ -37,13 +37,13 @@ async def opt_out(ctx: discord.ApplicationContext):
                 if line.strip():
                     opt_out_users.append(int(line))
     if ctx.author.id in opt_out_users:
-        await ctx.respond('Your message content is already off-track. To use other commands, please use the /opt-in command.', ephemeral=True)
+        await ctx.respond('Your message content is already off-track. To use other commands, please use the /opt-in command.')
     else:
         opt_out_users.append(ctx.author.id)
         with open(BASE_DIR / 'data' / 'opt-out-users.txt', 'w') as f:
             for user in opt_out_users:
                 f.write(str(user) + '\n')
-        await ctx.respond('This bot will not track your message content from now on. Most commands will no longer respond.', ephemeral=True)
+        await ctx.respond('This bot will not track your message content from now on. Most commands will no longer respond.')
 
 @bot.slash_command(name='opt-in')
 async def opt_in(ctx: discord.ApplicationContext):
@@ -59,8 +59,8 @@ async def opt_in(ctx: discord.ApplicationContext):
         with open(BASE_DIR / 'data' / 'opt-out-users.txt', 'w') as f:
             for user in opt_out_users:
                 f.write(str(user) + '\n')
-        await ctx.respond('This bot will now track the content of your messages. It will only be used to provide commands. Use the /privacy-policy command to view the privacy policy.', ephemeral=True)
+        await ctx.respond('This bot will now track the content of your messages. It will only be used to provide commands. Use the /privacy-policy command to view the privacy policy.')
     else:
-        await ctx.respond('This bot is already tracking your message content.', ephemeral=True)
+        await ctx.respond('This bot is already tracking your message content.')
 
 bot.run(GAATO_BOT_TOKEN if args.gaato_bot else CODERUNBOT_TOKEN)
