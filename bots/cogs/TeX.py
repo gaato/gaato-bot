ee593a4dc62ce6139a9a80bd50984258947aa3dc
gaato <g.furuya@gaato.net>
2023-06-13 17:22:35 +0900

    Fix TeX

--- bots/cogs/TeX.py
+++ bots/cogs/TeX.py
@@ -11,51 +11,25 @@ from .. import SUPPORT_SERVER_LINK, DeleteButton, LimitedSizeDict
 
 
 async def respond_core(
-        ctx: Union[discord.ApplicationContext, commands.Context],
-        code: str, file_type: str,
-        plain: Optional[bool],
-        spoiler: bool
-    ) -> Tuple[str, discord.Embed, Optional[discord.File]]:
-        url = 'http://127.0.0.1:9000/api'
-        params = {
-            'type': file_type,
-            'plain': plain,
-            'code': code,
-        }
-        async with aiohttp.ClientSession() as session:
-            async with session.post(url, json=params) as r:
-                if r.status == 200:
-                    result = await r.json()
-                else:
-                    embed = discord.Embed(
-                        title='Connection Error',
-                        description=f'{r.status}',
-                        color=0xff0000,
-                    )
-                    embed.set_author(
-                        name=ctx.author.name,
-                        icon_url=ctx.author.display_avatar.url,
-                    )
-                    return '', embed, None
-        match result['status']:
-            case 0:
-                embed = discord.Embed(color=0x008000)
-                embed.set_author(
-                    name=ctx.author.name,
-                    icon_url=ctx.author.display_avatar.url,
-                )
-                if file_type == 'png':
-                    embed.set_image(url='attachment://tex.png')
-                file = discord.File(
-                    io.BytesIO(base64.b64decode(result['result'])),
-                    filename='tex.png',
-                    spoiler=spoiler,
-                )
-                return '', embed, file
-            case 1:
+    ctx: Union[discord.ApplicationContext, commands.Context],
+    code: str, file_type: str,
+    plain: Optional[bool],
+    spoiler: bool
+) -> Tuple[str, discord.Embed, Optional[discord.File]]:
+    url = 'http://127.0.0.1:9000/api'
+    params = {
+        'type': file_type,
+        'plain': plain,
+        'code': code,
+    }
+    async with aiohttp.ClientSession() as session:
+        async with session.post(url, json=params) as r:
+            if r.status == 200:
+                result = await r.json()
+            else:
                 embed = discord.Embed(
-                    title='Rendering Error',
-                    description=f'```\n{result["error"]}\n```',
+                    title='Connection Error',
+                    description=f'{r.status}',
                     color=0xff0000,
                 )
                 embed.set_author(
@@ -63,26 +37,52 @@ async def respond_core(
                     icon_url=ctx.author.display_avatar.url,
                 )
                 return '', embed, None
-            case 2:
-                embed = discord.Embed(
-                    title='Timed Out',
-                    color=0xff0000,
-                )
-                embed.set_author(
-                    name=ctx.author.name,
-                    icon_url=ctx.author.display_avatar,
-                )
-                return '', embed, None
-            case _:
-                embed = discord.Embed(
-                    title='Unhandled Error',
-                    color=0xff0000,
-                )
-                embed.set_author(
-                    name=ctx.author.name,
-                    icon_url=ctx.author.display_avatar.url,
-                )
-                return f'Please report us!\n{SUPPORT_SERVER_LINK}', embed, None
+    match result['status']:
+        case 0:
+            embed = discord.Embed(color=0x008000)
+            embed.set_author(
+                name=ctx.author.name,
+                icon_url=ctx.author.display_avatar.url,
+            )
+            if file_type == 'png':
+                embed.set_image(url='attachment://tex.png')
+            file = discord.File(
+                io.BytesIO(base64.b64decode(result['result'])),
+                filename='tex.png',
+                spoiler=spoiler,
+            )
+            return '', embed, file
+        case 1:
+            embed = discord.Embed(
+                title='Rendering Error',
+                description=f'```\n{result["error"]}\n```',
+                color=0xff0000,
+            )
+            embed.set_author(
+                name=ctx.author.name,
+                icon_url=ctx.author.display_avatar.url,
+            )
+            return '', embed, None
+        case 2:
+            embed = discord.Embed(
+                title='Timed Out',
+                color=0xff0000,
+            )
+            embed.set_author(
+                name=ctx.author.name,
+                icon_url=ctx.author.display_avatar,
+            )
+            return '', embed, None
+        case _:
+            embed = discord.Embed(
+                title='Unhandled Error',
+                color=0xff0000,
+            )
+            embed.set_author(
+                name=ctx.author.name,
+                icon_url=ctx.author.display_avatar.url,
+            )
+            return f'Please report us!\n{SUPPORT_SERVER_LINK}', embed, None
 
 
 class TeXModal(discord.ui.Modal):
@@ -94,10 +94,11 @@ class TeXModal(discord.ui.Modal):
         self.add_item(discord.ui.InputText(
             label = 'Text' if plain else 'Code',
             placeholder='Input TeX code here',
-            multiline=True,
+            style=discord.InputTextStyle.long,
         ))
 
     async def callback(self, interaction: discord.Interaction):
+        await interaction.response.defer(invisible=False)
         content, embed, file = await respond_core(
             self.ctx,
             self.children[0].value,
@@ -105,7 +106,8 @@ class TeXModal(discord.ui.Modal):
             self.plain,
             self.spoiler,
         )
-        await interaction.respond(content=content, embed=embed, file=file)
+        print(content, embed, file)
+        await interaction.followup.send(content=content, embed=embed, file=file)
 
 class TeX(commands.Cog):
 
@@ -129,31 +131,31 @@ class TeX(commands.Cog):
 
     @commands.command()
     async def tex(self, ctx: commands.Context, *, code: str):
-        """TeX to image (in math mode)"""
+        """LaTeX to image (in math mode)"""
         m = await self.respond(ctx, code, 'png', False, False)
         self.user_message_id_to_bot_message[ctx.message.id] = m
 
     @commands.command()
     async def texp(self, ctx: commands.Context, *, code: str):
-        """TeX to image (out of math mode)"""
+        """LaTeX to image (out of math mode)"""
         m = await self.respond(ctx, code, 'png', True, False)
         self.user_message_id_to_bot_message[ctx.message.id] = m
 
     @commands.command()
     async def stex(self, ctx: commands.Context, *, code: str):
-        """TeX to spoiler image (in math mode)"""
+        """LaTeX to spoiler image (in math mode)"""
         m = await self.respond(ctx, code, 'png', False, True)
         self.user_message_id_to_bot_message[ctx.message.id] = m
 
     @commands.command()
     async def stexp(self, ctx: commands.Context, *, code: str):
-        """TeX to spoiler image (out of math mode)"""
+        """LaTeX to spoiler image (out of math mode)"""
         m = await self.respond(ctx, code, 'png', True, True)
         self.user_message_id_to_bot_message[ctx.message.id] = m
 
     @commands.command()
     async def texpdf(self, ctx: commands.Context, *, code: str):
-        """TeX to PDF (from preamble)"""
+        """LaTeX to PDF (from preamble)"""
         m = await self.respond(ctx, code, 'pdf', None, False)
         self.user_message_id_to_bot_message[ctx.message.id] = m
 
@@ -161,8 +163,9 @@ class TeX(commands.Cog):
         name='tex',
         description='TeX to image',
     )
-    async def tex_slash(self, ctx: commands.Context, plain: bool = False, spoiler: bool = False):
-        await TeXModal(ctx, plain, spoiler).start()
+    async def tex_slash(self, ctx: discord.ApplicationContext, plain: bool = False, spoiler: bool = False):
+        modal = TeXModal(ctx, plain, spoiler, title='LaTeX to Image')
+        await ctx.send_modal(modal)
 
 
 def setup(bot):
